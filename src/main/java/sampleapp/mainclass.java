2683bf3b91f5855af9dba1ff257f600a8de8937b
jhpachip <jhpachip@LIN25005206.corp.capgemini.com>
2021-08-16 16:20:20 +0530

    second commit

--- src/main/java/sampleapp/mainclass.java
+++ src/main/java/sampleapp/mainclass.java
@@ -1,10 +1,15 @@
 package sampleapp;
 
 public class mainclass {
+	static void display() {
+		System.out.println("This is the new function");
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Good afternoon");
+		
+		display();
 
 	}
 
