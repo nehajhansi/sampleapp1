374dae996d3afa1343bb353725f630eef067d9e2
jhpachip <jhpachip@LIN25005206.corp.capgemini.com>
2021-08-17 10:54:07 +0530

    display 2 method is added in main class

--- src/main/java/sampleapp/mainclass.java
+++ src/main/java/sampleapp/mainclass.java
@@ -4,6 +4,9 @@ public class mainclass {
 	static void display() {
 		System.out.println("This is the new function");
 	}
+	static void display2() {
+		System.out.println("Making some changes");
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
